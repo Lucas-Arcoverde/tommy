c439eddc6671e47d676280bf8d887a7e2dd48efb
Lucas A Melo <arcoverdemlucas@gmail.com>
2026-01-19 18:14:10 -0300

    Create main function accept flags with switch statement.

--- src/main.c
+++ src/main.c
@@ -5,5 +5,47 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
+		char * flag = argv[1];
+
+	switch (flag)
+	{
+		case "-h":
+		case "--help":
+			//display_help_message(program_name);
+			break;
+
+		case "-v":
+		case "--version":
+			//display_program_version();
+			break;
+
+		case "-a": //Create
+		case "--add":
+			//char * task = argv[2];
+			//add_task(task);
+			break;
+
+		case "-v": //Read
+		case "--view":
+			//view_tasks();
+			break;
+
+		case "-u": //Update
+		case "--update":
+			//char * task_index = argv[2];
+			//char * task = argv[3];
+			//update_task(task_index, task);
+			break;
+
+		case "-r": // Delete
+		case "--remove":
+			//char * task_index = argv[2];
+			//remove_task(task_index);
+			break;
+
+		default:
+			break;
+	}
+
 	return 0;
 }
