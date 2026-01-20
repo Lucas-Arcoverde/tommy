b9083d2bd2c0eb378511414ede02beeb7041faaa
Lucas A Melo <arcoverdemlucas@gmail.com>
2026-01-20 12:13:16 -0300

    Create add task function.

---
+++ src/task_process.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "task_process.h"
+
+#define MAX_TASKS 100
+#define MAX_CHARS 500
+
+char todo_list[MAX_TASKS][MAX_CHARS];
+int task_counter = 0;
+
+void add_task(char * task)
+{
+	if (task_counter == MAX_TASKS)
+	{
+		return;
+	}
+
+	else
+	{
+		strcpy(todo_list[task_counter], task);
+		printf(todo_list[task_counter]);
+		task_counter++;
+	}
+}
+
