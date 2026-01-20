62a264af75ab8e73cf418dd2072eb608d126d2ba
Lucas A Melo <arcoverdemlucas@gmail.com>
2026-01-19 21:26:52 -0300

    Create help message.

---
+++ src/utils.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "utils.h"
+
+void display_help_message(char * program_name)
+{
+	printf("\nTOMMY\n");
+	printf("A CLI To-Do list.\n\n");
+
+	printf("Usage: %s [FLAG] [ARGUMENTS]\n\n", program_name);
+
+	printf("Commands:\n");
+	printf(" -h  --help			Display this help message.\n");
+	printf(" --version			Display the current version of the program.\n");
+	printf(" -a  --add <TASK>		Add a new task to your list.\n");
+	printf(" -v  --view			Display your To-Do List.\n");
+	printf(" -u  --update <TASK_INDEX>	Change a task info.\n");
+	printf(" -r  --remove <TASK_INDEX>	Remove a task from your list.\n");
+}
+
+void display_program_version()
+{
+	return;
+}
+
---
+++ src/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void display_help_message(char * program_name);
+void display_program_version();
+
+#endif
