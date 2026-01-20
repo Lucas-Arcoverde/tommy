ff5ae8fc0c41c2c40e7cf89d6280ee1df6604b5a
Lucas A Melo <arcoverdemlucas@gmail.com>
2026-01-20 12:00:35 -0300

    Create file for task process.

---
+++ src/task_process.h
@@ -0,0 +1,9 @@
+#ifndef TASK_PROCESS
+#define TASK_PROCESS
+
+void add_task(char * task);
+void view_tasks();
+void update_task(char * task_index, char * task);
+void remove_task(char * task_index);
+
+#endif
