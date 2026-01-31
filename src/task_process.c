6f666330de42bd06b282c762a1b3de7d4c65322d
Lucas Arcoverde <arcoverdemlucas@gmail.com>
2026-01-31 11:52:20 -0300

    Create file task_process.c and create struct Task.

---
+++ src/task_process.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "task_process.h"
+
+#define MAX_TITLE_SIZE 100;
+#define MAX_DESCRIPTION_SIZE 500;
+#define MAX_DATE_SIZE 25;
+
+typedef struct {
+  char title[MAX_TITLE_SIZE];
+  char description[MAX_DESCRIPTION_SIZE];
+  char date[MAX_DATE_SIZE]
+} task;
+
