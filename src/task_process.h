0a9524899832c879153961f2ec21b1413c3b361c
Lucas Arcoverde <arcoverdemlucas@gmail.com>
2026-01-31 12:04:27 -0300

    Add function save_data and load_data in src/task_process.h.

--- src/task_process.h
+++ src/task_process.h
@@ -6,4 +6,7 @@ void view_tasks();
 void update_task(char * task_index, char * title, char * description, char * date);
 void remove_task(char * task_index);
 
+void save_data();
+void load_data();
+
 #endif TASK_PROCESS
