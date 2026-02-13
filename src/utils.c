480e671f6ae68da5a286e0ea69694b3ec1bd7d41
Lucas Arcoverde <arcoverdemlucas@gmail.com>
2026-02-13 12:55:25 -0300

    Re-add display program version function.

--- src/utils.c
+++ src/utils.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include "utils.h"
 
-void display_help_message(char * program_name)
-{
-	printf("\nTOMMY\n");
-	printf("A CLI To-Do list.\n\n");
-
-	printf("Usage: %s [FLAG] [ARGUMENTS]\n\n", program_name);
-
-	printf("Commands:\n");
-	printf(" -a  --add <TITLE> <DESC> <DATE>	Add a new task to your list.\n");
-	printf(" -v  --view				            View your to-do list.\n");
-	printf(" -u  --update <INDEX> <TITLE> <DESC> <DATE>\n");
-	printf("					                Update a task information.\n");
-	printf(" -r  --remove <INDEX>			    Remove a task from your list.\n");
-  printf(" -d  --view-details <INDEX>       	View a task details.\n");
-}
-
 void display_help_message(char *program_name)
 {
     printf("\nTOMMY\n");
@@ -32,3 +16,9 @@ void display_help_message(char *program_name)
     printf("  -d, --view-details <INDEX>               View task details\n");
 }
 
+void display_program_version()
+{
+  printf("Tommy\n");
+	printf("by Lucas Arcoverde.\n");
+	printf("\nVersion [ 2.13.2.26 ].\n");
+}
