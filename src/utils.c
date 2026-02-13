81bdb68567e8fbf751f87beae2aec51f0c1b1d2e
Lucas Arcoverde <arcoverdemlucas@gmail.com>
2026-02-13 12:37:21 -0300

    Update help and version messages.

--- src/utils.c
+++ src/utils.c
@@ -10,16 +10,17 @@ void display_help_message(char * program_name)
 
 	printf("Commands:\n");
 	printf(" -a  --add <TITLE> <DESC> <DATE>	Add a new task to your list.\n");
-	printf(" -v  --view				View your to-do list.\n");
+	printf(" -v  --view				                View your to-do list.\n");
 	printf(" -u  --update <INDEX> <TITLE> <DESC> <DATE>\n");
-	printf("					Update a task information.\n");
-	printf(" -r  --remove <INDEX>			Remove a task from your list.\n");
+	printf("					                        Update a task information.\n");
+	printf(" -r  --remove <INDEX>			        Remove a task from your list.\n");
+  printf(" -d  --view-details <INDEX>       View a task details.\n");
 }
 
 void display_program_version()
 {
 	printf("Tommy\n");
 	printf("by Lucas Arcoverde.\n");
-	printf("\nVersion [ 2.7.2.26 ].\n"); // version 2 realise February 2nd 2026.
+	printf("\nVersion [ 2.13.2.26 ].\n");
 }
 
