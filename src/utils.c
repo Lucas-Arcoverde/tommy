45487d1bb888df39050d5af7fa226725747a44df
Lucas Arcoverde <arcoverdemlucas@gmail.com>
2026-02-13 12:45:05 -0300

    Fix help message format.

--- src/utils.c
+++ src/utils.c
@@ -10,17 +10,25 @@ void display_help_message(char * program_name)
 
 	printf("Commands:\n");
 	printf(" -a  --add <TITLE> <DESC> <DATE>	Add a new task to your list.\n");
-	printf(" -v  --view				                View your to-do list.\n");
+	printf(" -v  --view				            View your to-do list.\n");
 	printf(" -u  --update <INDEX> <TITLE> <DESC> <DATE>\n");
-	printf("					                        Update a task information.\n");
-	printf(" -r  --remove <INDEX>			        Remove a task from your list.\n");
-  printf(" -d  --view-details <INDEX>       View a task details.\n");
+	printf("					                Update a task information.\n");
+	printf(" -r  --remove <INDEX>			    Remove a task from your list.\n");
+  printf(" -d  --view-details <INDEX>       	View a task details.\n");
 }
 
-void display_program_version()
+void display_help_message(char *program_name)
 {
-	printf("Tommy\n");
-	printf("by Lucas Arcoverde.\n");
-	printf("\nVersion [ 2.13.2.26 ].\n");
+    printf("\nTOMMY\n");
+    printf("A CLI To-Do list.\n\n");
+
+    printf("Usage: %s [FLAG] [ARGUMENTS]\n\n", program_name);
+
+    printf("Commands:\n");
+    printf("  -a, --add <TITLE> <DESC> <DATE>         Add a new task\n");
+    printf("  -v, --view                               View your to-do list\n");
+    printf("  -u, --update <INDEX> <TITLE> <DESC> <DATE>  Update a task\n");
+    printf("  -r, --remove <INDEX>                     Remove a task\n");
+    printf("  -d, --view-details <INDEX>               View task details\n");
 }
 
